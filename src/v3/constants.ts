16dfcc4aa90348ffeda0cf4f1e8c0164a5cbc0e1
yubai <luoyi@mouji.net>
2026-03-22 19:25:37 +0800

    feat: 移除文件名常量

--- src/v3/constants.ts
+++ src/v3/constants.ts
@@ -3,18 +3,13 @@ export const SCHEMA_BASE_URL = 'https://weme-ui.github.io/schema'
 
 // Registry
 export const REGISTRY_SCHEMA_URL = `${SCHEMA_BASE_URL}/${SCHEMA_VERSION}/registry.schema.json`
-export const REGISTRY_SCHEMA_NAME = 'registry.json'
 
 // Config
 export const CONFIG_SCHEMA_URL = `${SCHEMA_BASE_URL}/${SCHEMA_VERSION}/config.schema.json`
-export const CONFIG_SCHEMA_NAME = 'weme.config.json'
 
 // Manifest
 export const MANIFEST_SCHEMA_URL = `${SCHEMA_BASE_URL}/${SCHEMA_VERSION}/manifest.schema.json`
-export const MANIFEST_SCHEMA_NAME = 'manifest.json'
 
 // Template
 export const TEMPLATE_SCHEMA_URL = `${SCHEMA_BASE_URL}/${SCHEMA_VERSION}/template.schema.json`
-export const TEMPLATE_SCHEMA_NAME = 'template.json'
 export const TEMPLATE_INDEX_URL = `${SCHEMA_BASE_URL}/${SCHEMA_VERSION}/template-index.schema.json`
-export const TEMPLATE_INDEX_NAME = 'index.json'
